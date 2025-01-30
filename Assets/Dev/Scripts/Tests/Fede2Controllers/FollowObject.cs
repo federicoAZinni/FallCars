e5fa890f3903c6db96fff335b5bafc19385da616
Fede <fede@MBP-de-Fede.communityfibre.co.uk>
2025-01-30 22:39:39 +0000

    New kitchen

--- Assets/Dev/Scripts/Tests/Fede2Controllers/FollowObject.cs
+++ Assets/Dev/Scripts/Tests/Fede2Controllers/FollowObject.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class FollowObject : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] bool followRotation = true;
     
     // Update is called once per frame
     void Update()
     {
         transform.position = target.position;
+        if (!followRotation) return;
+        transform.rotation = Quaternion.Euler(0, target.rotation.eulerAngles.y, 0);
     }
 }
